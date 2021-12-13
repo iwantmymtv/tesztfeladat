7dfd1abebda2f34ed868325a47181b1a652da337
iwantmymtv <44582992+iwantmymtv@users.noreply.github.com>
2021-12-13 12:48:05 +0100

    input validations

--- 2.py
+++ 2.py
@@ -18,9 +18,9 @@ import numpy as np
 class Queen:
     def __init__(self,n:int,r_q:int,c_q:int,obstacles:Optional[List[List[int]]]= None) -> None:
         self.n = n
-        self.r_q = r_q
-        self.c_q = c_q
-        self.obstacles = obstacles
+        self.r_q = self._is_valid_queen_position(r_q)
+        self.c_q = self._is_valid_queen_position(c_q)
+        self.obstacles = self._is_valid_obstacles(obstacles)
 
         self.ups = []
         self.downs = []
@@ -32,6 +32,21 @@ class Queen:
         self.downlefts = []
 
         self.possible_moves = []
+
+    def _is_valid_obstacles(self,obstacles:Optional[List[List[int]]]) -> Optional[List[List[int]]]:
+        if len(obstacles) > (self.n * self.n)-1:
+            raise ValueError(f"Too many obsticles")
+        if obstacles:
+            for i in obstacles:
+                for j in i:
+                    if j <= 0 or j > self.n:
+                        raise ValueError(f"Invalid obstacle position,values must be between 1 and {self.n}")
+        return obstacles
+
+    def _is_valid_queen_position(self,p:int) -> int:
+        if p <= 0 or p > self.n: 
+            raise ValueError(f"Invalid queen position,values must be between 1 and {self.n}")
+        return p
 
     def check_if_obstacle(self,pos:List[int]) -> bool:
         if self.obstacles:
@@ -117,9 +132,10 @@ class Queen:
         qp = self.convert_position([self.r_q,self.c_q])
         board[qp[0],qp[1]] = 9
         #obstacles
-        for o in self.obstacles:
-           pos = self.convert_position(o) 
-           board[pos[0],pos[1]] = 2
+        if self.obstacles:
+            for o in self.obstacles:
+                pos = self.convert_position(o) 
+                board[pos[0],pos[1]] = 2
         #possible moves
         for p in self.possible_moves:
            pos = self.convert_position(p) 
@@ -130,10 +146,7 @@ class Queen:
     def all_possible_moves(self) -> int:
         return len(self.possible_moves)
 
-q = Queen(8,5,5,[[3,5],[7,7]])
+q = Queen(8,3,5,[[5,4],[3,1],[8,8]])
 q.init_values()
 q.print_table()
 print("Number of possible moves: ",q.all_possible_moves)
-
-
-
